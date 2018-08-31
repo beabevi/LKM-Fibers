625ea1b6e3683b25b5111aa9dbfb9335075c586b
Beatrice Bevilacqua <beatricebevilacqua1995@gmail.com>
2018-08-31 12:25:30 +0200

    fixed formatting

--- lib/fibers.c
+++ lib/fibers.c
@@ -3,7 +3,7 @@
 #define assert_fibers_open \
         do{              \
             if (__fibers_file < 0){ \
-                printf("fibers file is not open"); \
+                printf("fibers file is not open\n"); \
             } \
         }while(0);
 
@@ -13,10 +13,9 @@ void to_fiber(void){
     __fibers_file = open("/dev/" DEVICE_NAME, O_NONBLOCK);
     assert_fibers_open
 
-    printf("IOCTL_TO_FIB = %lu\n", IOCTL_TO_FIB);
     int ret = ioctl(__fibers_file, IOCTL_TO_FIB, 0);
     if (ret < 0){
-        printf("Error in ioctl");
+        printf("Error in ioctl\n");
     }
 }
 void create_fiber(size_t stack_size, void (*entry_point)(void*), void* param){
@@ -24,7 +23,7 @@ void create_fiber(size_t stack_size, void (*entry_point)(void*), void* param){
 
     int ret = ioctl(__fibers_file, IOCTL_CREATE_FIB, 0);
     if (ret < 0){
-        printf("Error in ioctl");
+        printf("Error in ioctl\n");
     }
 }
 void switch_fiber(void* fid){
@@ -32,7 +31,7 @@ void switch_fiber(void* fid){
 
     int ret = ioctl(__fibers_file, IOCTL_SWITCH_FIB, fid);
     if (ret < 0){
-        printf("Error in ioctl");
+        printf("Error in ioctl\n");
     }
 }
 long fls_alloc(void){
@@ -40,7 +39,7 @@ long fls_alloc(void){
 
     int ret = ioctl(__fibers_file, IOCTL_FLS_ALLOC, 0);
     if (ret < 0){
-        printf("Error in ioctl");
+        printf("Error in ioctl\n");
     }
 }
 bool fls_free(long index){
@@ -48,7 +47,7 @@ bool fls_free(long index){
 
     int ret = ioctl(__fibers_file, IOCTL_FLS_FREE, index);
     if (ret < 0){
-        printf("Error in ioctl");
+        printf("Error in ioctl\n");
     }
 }
 void fls_set(long index, long long value){
@@ -56,7 +55,7 @@ void fls_set(long index, long long value){
 
     int ret = ioctl(__fibers_file, IOCTL_FLS_SET, 0);
     if (ret < 0){
-        printf("Error in ioctl");
+        printf("Error in ioctl\n");
     }
 }
 long long fls_get(long index){
@@ -64,6 +63,6 @@ long long fls_get(long index){
 
     int ret = ioctl(__fibers_file, IOCTL_FLS_GET, index);
     if (ret < 0){
-        printf("Error in ioctl");
+        printf("Error in ioctl\n");
     }
 }
